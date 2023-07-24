6aac29efff4679f846d08a107899b1bc58f86121
junhyeonp <psjking1229@naver.com>
2023-07-24 16:38:51 +0900

    "commit"

---
+++ src/controllers/rentalControllers.js
@@ -0,0 +1,39 @@
+import Notice from "../models/notice.js";
+
+export const rentalNoticeWrite = async (req, res) => {
+  const { title, description, writer } = req.body;
+  console.log(title, description, writer);
+  console.log(title, description, writer);
+  try {
+    const notice = await Notice.create({
+      title,
+      description,
+      writer,
+      createdAt: Date.now(),
+    });
+    res.json({ ok: "true", notice });
+  } catch (error) {
+    res.json({ ok: "false", error });
+  }
+};
+
+export const rentalNotice = async (req, res) => {
+  try {
+    const notices = await Notice.find({}).sort({ createdAt: -1 });
+    const counts = await Notice.count();
+    res.json({ ok: "true", notices, counts });
+  } catch (error) {
+    res.json({ ok: "false", error });
+  }
+};
+
+// 게시판 디테일
+export const rentalNoticeDetail = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const detail = await Notice.findById(id);
+    res.json({ ok: "true", detail });
+  } catch (error) {
+    res.json({ ok: "false", error });
+  }
+};
--- src/routers/rentalRouter.js
+++ src/routers/rentalRouter.js
@@ -1,7 +1,14 @@
 import express from "express";
+import {
+  rentalNotice,
+  rentalNoticeDetail,
+  rentalNoticeWrite,
+} from "../controllers/rentalControllers.js";
 
 const rentalRouter = express.Router();
 
-rentalRouter.get("/notice", (req, res) => res.json({ data: "notice" }));
+rentalRouter.get("/notice", rentalNotice);
+rentalRouter.post("/notice-write", rentalNoticeWrite);
+rentalRouter.get("/notice-detail/:id", rentalNoticeDetail);
 
 export default rentalRouter;
