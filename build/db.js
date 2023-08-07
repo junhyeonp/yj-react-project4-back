ff6bb992b739b27c724443d6f2ef8b38f90dc912
junhyeonpark <psjking1229@naver.com>
2023-08-07 11:22:43 +0900

    commit

---
+++ build/db.js
@@ -0,0 +1,15 @@
+"use strict";
+
+require("dotenv/config");
+var _mongoose = _interopRequireDefault(require("mongoose"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_mongoose["default"].connect(process.env.DB_URL);
+var db = _mongoose["default"].connection;
+var handleOpen = function handleOpen() {
+  return console.log("✔ Connected to DB");
+};
+db.on("error", function (error) {
+  return console.log("DB Error", error);
+});
+db.once("open", handleOpen);
+/// good!
